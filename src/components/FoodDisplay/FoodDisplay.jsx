75eee3464a112e82c5d7200f5d34b7b3c9c9aca4
shoxvrz <shoxruzabdurasulov7@gmail.com>
2024-08-05 21:07:50 +0500

    ff

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useGetAllDataQuery } from '../../toolkit/Food/foodApi';
-import FoodItem from '../FoodItem/FoodItem';
-import './FoodDisplay.scss';
+import React from "react";
+import { useSelector } from "react-redux";
+import { useGetAllDataQuery } from "../../toolkit/Food/foodApi";
+import FoodItem from "../FoodItem/FoodItem";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 const FoodDisplay = () => {
   const { data, error, isLoading } = useGetAllDataQuery();
   const searchQuery = useSelector((state) => state.search.searchedItem);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <Box sx={{ display: "flex",  alignItems: 'center', justifyContent: 'center'}}>
+        <CircularProgress />
+      </Box>
+    );
   }
 
   if (error) {
     return <div>Error: {error.message}</div>;
   }
 
-  const filteredItems = data.filter(item =>
+  const filteredItems = data.filter((item) =>
     item.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
-    <div className='dispFood'>
+    <div className="dispFood">
       <h2>Sizga yaqin eng yaxshi taomlar</h2>
       <div className="dispFood__list">
         {filteredItems.map((item, i) => (
